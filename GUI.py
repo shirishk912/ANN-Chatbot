dface51371a4e1f9a9df0ad631aee2b01f50f75e
Shirish Kandra <shirishk912@gmail.com>
2021-12-14 18:48:20 +0000

    Dataset updated. Dissertation commit

--- GUI.py
+++ GUI.py
@@ -1,69 +1,73 @@
+# CHATBOT GUI COMPONENT
+
+# IMPORTING THE NECESSARY LIBRARIES FOR THE TASK
+
 import nltk
-import os   
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
-
-from keras.models import load_model
-model = load_model('chatbot_model.h5')
 import json
 import random
-
 from tkHyperLinkManager import HyperlinkManager
 import webbrowser
 from functools import partial
+from keras.models import load_model
+import tkinter
+from tkinter import *
 
+# TO DISABLE TENSORFLOW FROM DISPLAYING ERROR MESSAGES RELATING TO GPU ALLOCATION
+# **Not related to context of chatbot model implementation**
+import os  
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-BG_GRAY = "#ABB2B9"
+BORDERS = "#4F4F4F"
 BG_COLOR = "#17202A"
 TEXT_COLOR = "#EAECEE"
-
+SEND_BTN = "#90EE90"
 FONT = "Helvetica 14"
 FONT_BOLD = "Helvetica 13 bold"
 
+# LOADING THE SAVED MODEL, INTENTS FILE, DESERIALIZING WORDS AND CLASSES FILE
+model = load_model('model.h5')
 intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
 
-
+# PRE-PROCESSING (TOKENIZING AND LEMMATIZING) USER INPUT TEXT
 def clean_up_sentence(sentence):
-    # tokenize the pattern - split words into array
     sentence_words = nltk.word_tokenize(sentence)
-    # stem each word - create short form for word
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+# PERFORMING BAG OF WORDS TECHNIQUE ON USER INPUT PATTERN AND WORDS LIST
 
 def bow(sentence, words, show_details=True):
-    # tokenize the pattern
     sentence_words = clean_up_sentence(sentence)
-    # bag of words - matrix of N words, vocabulary matrix
     bag = [0]*len(words)  
     for s in sentence_words:
         for i,w in enumerate(words):
             if w == s: 
-                # assign 1 if current word is in the vocabulary position
                 bag[i] = 1
                 if show_details:
                     print ("found in bag: %s" % w)
     return(np.array(bag))
 
+# PREDICTING THE CLASS(TAG) TO WHICH THE USER INPUT BELONGS TO
 def predict_class(sentence, model):
-    # filter out predictions below a threshold
     p = bow(sentence, words,show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
+    # FILTERING OUT PREDICTIONS BELOW A THRESHOLD
     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
-    # sort by strength of probability
+    # SORTING BY STRENGTH OF PROBABILITY OF THE PREDICTION
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
     return return_list
 
+# GETTING THE PREDICTED RESPONSE
 def getResponse(ints, intents_json):
     tag = ints[0]['intent']
     list_of_intents = intents_json['intents']
@@ -73,12 +77,13 @@ def getResponse(ints, intents_json):
             break
     return result
 
+# PREDICTING AND GETTING THE RESPONSE AND SENT THE RESPONSE TO DISPLAY
 def chatbot_response(msg):
     ints = predict_class(msg, model)
     res = getResponse(ints, intents)
     return res
 
-# Retrieving hyperlinks
+# RETRIEVING HYPERLINKS FOR THE PREDICTED RESPONSE
 
 def getResponseLink(ints, intents_json):
     tag = ints[0]['intent']
@@ -97,66 +102,54 @@ def chatbot_response_link(msg):
 
 # GRAPHICAL USER INTERFACE (GUI) USING TKINTER LIBRARY
 
-import tkinter
-from tkinter import *
 
 
-# Define a callback function
-
+# DEFINING A CALLBACK FUNCTION TO OPEN A NEW BROWSER TAB
 def callback(url):
    webbrowser.open_new_tab(url)
  
-
+# GETTING USER INPUT
 def send():
     msg = EntryBox.get("1.0",'end-1c').strip()
     EntryBox.delete("0.0",END)
-
     if msg != '':
         ChatLog.config(state=NORMAL)
-        ChatLog.insert(END, "STUDENT: " + msg + '\n\n')
-    
+        ChatLog.insert(END, "STUDENT: \n\n" + msg + '\n\n')
+        
+        # RETRIEVING AND DISPLAYING THE RESPONSE
         res = chatbot_response(msg)
         link= chatbot_response_link(msg)
-        ChatLog.insert(END, "CHATBOT: " + res)
-        ChatLog.insert(END, "here" + '\n\n', hyperlink.add(partial(webbrowser.open,link)))
-            
+        ChatLog.insert(END, "CHATBOT: \n\n" + res)
+        ChatLog.insert(END, "here" + '\n\n', hyperlink.add(partial(webbrowser.open,link)))     
         ChatLog.config(state=DISABLED)
         ChatLog.yview(END)
 
-# CREATING CHAT WINDOW BASE STRUCTURE
+# CREATING CHAT WINDOW BASE GUI STRUCTURE
 
 base = Tk()
 base.title("Swansea University Chatbot")
 base.configure(width=770, height=550, bg=BG_COLOR)
 base.resizable(width=False, height=False)
-
-head_label = Label(base, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome", font=FONT_BOLD, pady=10)
-
-line = Label(base, width=750, bg=BG_GRAY)
-
-#Create Chat window
-ChatLog = Text(base, width=20, height=2, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, padx=5, pady=5)
+# HEAD LABEL
+head_label = Label(base, bg="#FDF5E6", fg="#33A1C9", text="Welcome", font=FONT_BOLD, pady=10)
+# A DIVIDER SEPERATING HEAD LABEL AND CHATLOG
+line = Label(base, width=750, bg=BORDERS)
+# CHATLOG FRAME
+ChatLog = Text(base, width=20, height=2, bg="#292421", fg=TEXT_COLOR, font=FONT, padx=5, pady=5)
 ChatLog.config(cursor="arrow", state=DISABLED)
-
 hyperlink= HyperlinkManager(ChatLog)
-
-#Bind scrollbar to Chat window
+# BINDING SCROLLBAR TO THE CHATLOG FRAME
 scrollbar = Scrollbar(ChatLog)
 scrollbar.configure(command=ChatLog.yview)
-
-# bottom label
-bottom_label = Label(base, bg=BG_GRAY, height=80)
-
-#Create the box to enter message
-EntryBox = Text(bottom_label, bg="#2C3E50", fg=TEXT_COLOR, font=FONT)
+# A DIVIDER SEPERATING CHATLOG AND INPUT AREA
+bottom_label = Label(base, bg=BORDERS, height=80)
+# INPUT ENTRY BOX
+EntryBox = Text(bottom_label, bg="#FFF5EE", fg="#1E1E1E", font=FONT)
 EntryBox.focus()
-
-#Create Button to send message
-SendButton = Button(bottom_label, text="Send", font=FONT_BOLD, width=20, bg=BG_GRAY,
+# SEND BUTTON
+SendButton = Button(bottom_label, text="Send", font=FONT_BOLD, width=20, bg=SEND_BTN,
                     command= send )
-
-
-#Place all components on the screen
+# PLACING ALL THE COMPONENTS IN THE BASE STRUCTURE
 head_label.place(relwidth=1)
 line.place(relwidth=1, rely=0.07, relheight=0.012)
 ChatLog.place(relheight=0.745, relwidth=1, rely=0.08)
@@ -164,5 +157,4 @@ scrollbar.place(relheight=1, relx=0.974)
 bottom_label.place(relwidth=1, rely=0.825)
 EntryBox.place(relwidth=0.74, relheight=0.06, rely=0.008, relx=0.011)
 SendButton.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
-
 base.mainloop()
--- train_bot.py
+++ train_bot.py
@@ -1,18 +1,26 @@
+# CHATBOT ANN MODEL COMPONENT
+
+# IMPORTING THE NECESSARY LIBRARIES FOR THE TASK
+
 import nltk
-import os
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import json
 import pickle
-
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
 import random
 
+# TO DISABLE TENSORFLOW FROM DISPLAYING ERROR MESSAGES RELATING TO GPU ALLOCATION
+# **Not related to context of chatbot model implementation**
+
+import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+# VARIABLES INITIALIZATION AND LOADING INTENTS DATASET
+ 
 words=[]
 classes = []
 documents = []
@@ -20,67 +28,65 @@ ignore_words = ['?','!']
 data_file = open('intents.json').read()
 intents = json.loads(data_file)
 
+# PRE-PROCESSING THE INTENTS TRAINING DATASET
 
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-
-        #tokenize each word
+        # TOKENIZING EACH WORD OF THE PATTERN
         w = nltk.word_tokenize(pattern)
         words.extend(w)
-        #add documents in the corpus
+        # PAIRING EACH WORD OF THE PATTERN WITH ITS RESPECTIVE TAG 
         documents.append((w, intent['tag']))
-
-        # add to our classes list
+        # STORING EACH UNIQUE TAG
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-# lemmaztize and lower each word and remove duplicates
+# LEMMATIZING, IGNORING SPECIAL CHARACTERS AND CONVERTING EACH WORD INTO LOWERCASE
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
-# sort classes
+# SORTING ELEMENTS OF THE CLASS IN ASCENDING ORDER
 classes = sorted(list(set(classes)))
-# documents = combination between patterns and intents
-print (len(documents), "documents")
-# classes = intents
+# DISPLAYING THE DOCUMENTS(PAIR BETWEEN PATTERN AND ITS TAG) OF THE DATA
+print (len(documents), "documents", documents)
+# DISPLAYING THE TAGS OF THE DATA
 print (len(classes), "classes", classes)
-# words = all words, vocabulary
+# DISPLAYING ALL THE UNIQUE WORDS OF ALL THE PATTERNS IN THE DATA
 print (len(words), "unique lemmatized words", words)
 
-
+# SERIALIZING THE WORDS AND CLASSES OBJECTS WHICH IS LATER USED BY GUI.PY
 pickle.dump(words,open('words.pkl','wb'))
 pickle.dump(classes,open('classes.pkl','wb'))
 
-# create our training data
+# CREATING TRAINING DATA
 training = []
-# create an empty array for our output
 output_empty = [0] * len(classes)
-# training set, bag of words for each sentence
+# CREATING BAG OF WORDS FOR EACH SENTENCE OF THE TRAINING SET
 for doc in documents:
-    # initialize our bag of words
     bag = []
-    # list of tokenized words for the pattern
+    # LIST OF TOKENIZED WORDS FOR A PATTERN
     pattern_words = doc[0]
-    # lemmatize each word - create base word, in attempt to represent related words
     pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
-    # create our bag of words array with 1, if word match found in current pattern
+    # UPDATE BAG OF WORDS WITH 1 IF MATCH FOUND IN CURRENT POSITION, IF NOT APPEND 0
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
-    
-    # output is a '0' for each tag and '1' for current tag (for each pattern)
+    # FOR EACH PATTERN, APPEND 1 TO OUTPUT FOR CURRENT TAG, 0 FOR OTHER TAGS  
     output_row = list(output_empty)
     output_row[classes.index(doc[1])] = 1
-    
     training.append([bag, output_row])
-# shuffle our features and turn into np.array
+# SHUFFLING THE DATA ALONG BAG OF WORDS AXIS AND CONVERTING INTO NUMPY ARRAY
 random.shuffle(training)
 training = np.array(training, dtype=object)
-# create train and test lists. X - patterns, Y - intents
+# CREATING TRAINING AND TESTING LISTS
+# TRAIN_X GIVES THE INPUT SIZE OF THE MODEL i.e. NUMBER OF PATTERNS
+# TRAIN_Y GIVES THE OUTPUT SIZE OF THE MODEL i.e. NUMBER OF TAGS
 train_x = list(training[:,0])
 train_y = list(training[:,1])
-print("Training data created")
+print(len(train_x[0]))
+print(len(train_y[0]))
+print("\nTraining data is created")
 
-
-# Model creation
+# DEFINING THE MODEL
+# 1 INPUT LAYER, 3 HIDDEN LAYERS AND 1 OUTPUT LAYER
 model = Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
@@ -88,12 +94,19 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+# COMPILING THE MODEL
+# OPTIMIZER - STOCHASTIC GRADIENT DESCENT WITH NESTEROV ACCELERATED GRADIENT
 sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-#fitting and saving the model 
+# FITTING THE TRAINING DATA
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=300, batch_size=4, verbose=1)
-model.save('chatbot_model.h5', hist)
 
-print("model created")
+# METRICS
+score = model.evaluate(np.array(train_x), np.array(train_y))
+print(f"\nTest accuracy: {score[1]*100:0.2f}%")
+
+# SAVING THE MODEL
+model.save('model.h5', hist)
+
+print("\n Model is Created")
